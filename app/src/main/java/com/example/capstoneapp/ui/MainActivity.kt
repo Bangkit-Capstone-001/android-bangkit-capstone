41c0346e065298e0b233525886d14d4956ad3e6a
Marcelinus Gerardo Ari Nugroho <marcelinus.gerardo@ui.ac.id>
2024-04-23 20:26:04 +0700

    Project init

---
+++ app/src/main/java/com/example/capstoneapp/ui/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.capstoneapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.capstoneapp.R
+import com.example.capstoneapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.bottom_1 -> {
+                    replaceFragment(Feature01Fragment())
+                    true
+                }
+                R.id.bottom_2 -> {
+                    replaceFragment(Feature02Fragment())
+                    true
+                }
+                R.id.bottom_3 -> {
+                    replaceFragment(Feature03Fragment())
+                    true
+                }
+                R.id.bottom_4 -> {
+                    replaceFragment(Feature04Fragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        // default fragment
+        replaceFragment(Feature01Fragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(binding.frameLayout.id, fragment).commit()
+    }
+}
