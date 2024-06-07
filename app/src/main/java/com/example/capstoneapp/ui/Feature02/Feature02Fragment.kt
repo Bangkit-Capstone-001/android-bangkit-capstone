df509b359b086014ad40300c19b681fc8c8b384a
Aliefnaufal <18221105@std.stei.itb.ac.id>
2024-06-07 14:02:31 +0700

    setup enterance on adding a workout plan

--- app/src/main/java/com/example/capstoneapp/ui/Feature02/Feature02Fragment.kt
+++ app/src/main/java/com/example/capstoneapp/ui/Feature02/Feature02Fragment.kt
@@ -1,21 +1,37 @@
 package com.example.capstoneapp.ui.Feature02
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.capstoneapp.R
+import com.example.capstoneapp.databinding.FragmentFeature02Binding
+import com.example.capstoneapp.ui.Feature02.WorkoutPreference.WorkoutPreferenceActivity
 
 class Feature02Fragment : Fragment() {
+
+    private var _binding: FragmentFeature02Binding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feature02, container, false)
+        _binding = FragmentFeature02Binding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {}
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setAction()
+    }
+
+    private fun setAction() {
+        binding.mainFragment2IvAddWorkoutPlanButton.setOnClickListener {
+            startActivity(Intent(activity, WorkoutPreferenceActivity::class.java))
+        }
+    }
 }
--- app/src/main/java/com/example/capstoneapp/ui/Feature02/WorkoutPreference/WorkoutPreferenceActivity.kt
+++ app/src/main/java/com/example/capstoneapp/ui/Feature02/WorkoutPreference/WorkoutPreferenceActivity.kt
@@ -6,9 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.capstoneapp.R
+import com.example.capstoneapp.databinding.ActivityWorkoutPreferenceBinding
 
 class WorkoutPreferenceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkoutPreferenceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWorkoutPreferenceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setAction()
+    }
+
+    private fun setAction() {
+        binding.workoutPreferenceIvBackButton.setOnClickListener {
+            finish()
+        }
     }
 }
