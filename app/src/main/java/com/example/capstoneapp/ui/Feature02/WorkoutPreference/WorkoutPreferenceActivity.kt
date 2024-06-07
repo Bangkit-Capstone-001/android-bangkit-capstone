4f133cc2e957971508e43fb18d4f886af8b77b5d
Aliefnaufal <18221105@std.stei.itb.ac.id>
2024-06-07 09:41:48 +0700

    Landing page of workout preference

---
+++ app/src/main/java/com/example/capstoneapp/ui/Feature02/WorkoutPreference/WorkoutPreferenceActivity.kt
@@ -0,0 +1,14 @@
+package com.example.capstoneapp.ui.Feature02.WorkoutPreference
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.capstoneapp.R
+
+class WorkoutPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
