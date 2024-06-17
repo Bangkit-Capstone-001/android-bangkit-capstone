2f04b6fad82e99dd8e580bc655abc5b2cb43339e
Aliefnaufal <18221105@std.stei.itb.ac.id>
2024-06-17 12:43:49 +0700

    adjusting time

--- app/src/main/java/com/example/capstoneapp/ui/Feature02/WorkoutFinish/WorkoutFinishActivity.kt
+++ app/src/main/java/com/example/capstoneapp/ui/Feature02/WorkoutFinish/WorkoutFinishActivity.kt
@@ -6,9 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.capstoneapp.R
+import com.example.capstoneapp.databinding.ActivityWorkoutFinishBinding
 
 class WorkoutFinishActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWorkoutFinishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityWorkoutFinishBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
